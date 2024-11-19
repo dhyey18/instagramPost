8fd84e8e98d72c38bff41ebcda5240c5344965ad
dhyey18 <pateldhyey64@gmail.com>
2024-11-19 13:37:54 +0530

    code update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import connectDB from "./database/connection.js";
-import app from "./app.js";
+// import connectDB from "./database/connection.js";
+// import app from "./app.js";
 
-const PORT = 3000;
-const db_url = `mongodb+srv://DhyeyPatel:DhyeyPatel%4018@atlascluster.etej1ka.mongodb.net/instagram`;
+// const PORT = 3000;
+// const db_url = `mongodb+srv://DhyeyPatel:DhyeyPatel%4018@atlascluster.etej1ka.mongodb.net/instagram`;
 
-connectDB(db_url).then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+// connectDB(db_url).then(() => {
+//   console.log("Connected to MongoDB");
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
+
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
