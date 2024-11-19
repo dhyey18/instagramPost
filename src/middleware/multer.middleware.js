f66036d60500e7b1c081171298ce93918cfdef6f
dhyey18 <pateldhyey64@gmail.com>
2024-11-19 13:51:01 +0530

    upload changes

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,11 +1,19 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
-const uploadDir = path.join("", "uploads");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Resolve the absolute path to the 'uploads' directory
+const uploadDir = path.resolve(__dirname, "uploads");
+
+// Check if the 'uploads' directory exists and create it if it doesn't
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
+
 // Configure Multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
